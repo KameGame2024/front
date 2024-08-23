4e3b0e538fd419716c2cd6bf3106b0b49c526dd5
Anna Ospina Bedoya <anospina@unal.edu.co>
2024-08-23 00:45:31 -0500

    cambios en el responsive

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { FaHome, FaPlus, FaTag } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa'; // Importing the menu icon
 import BarraBusqueda from '../BarraBusqueda';
 import Menu from '../Menu';
 import './Header.css';
@@ -10,9 +10,14 @@ function Header() {
     const location = useLocation();
     const { actualizarBusqueda } = useContext(GlobalContext);
     const showSearchBar = location.pathname === '/cartas' || location.pathname === '/paquetes';
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // Initialize with false
 
     const handleSearch = (query) => {
         actualizarBusqueda(query);
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
@@ -30,8 +35,12 @@ function Header() {
                         <img src="/img/icons/shopping-bag.png" alt="Carrito" />
                     </NavLink>
                 </div>
+                <FaBars className="menu-icon" onClick={toggleMenu} />
             </header>
-            <Menu />
+            {/* Show the Menu component when the icon is clicked */}
+            <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            {/* Background overlay when the menu is open on mobile */}
+            {isMenuOpen && <div className="menu-overlay" onClick={toggleMenu}></div>}
         </div>
     );
 }
--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,16 +1,20 @@
-// src/componentes/Menu.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Menu.css';
 
-function Menu() {
+function Menu({ isMenuOpen, setIsMenuOpen }) {
+    // Close the menu when an item is clicked
+    const handleCloseMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
-        <nav className="menu">
-            <NavLink to="/cartas" className="menu-item">CARTAS</NavLink>
-            <NavLink to="/paquetes" className="menu-item">PAQUETES</NavLink>
-            <NavLink to="/centro-de-combate" className="menu-item">CENTRO DE COMBATE</NavLink>
-            <NavLink to="/mi-coleccion" className="menu-item">MI COLECCION</NavLink>
-            <NavLink to="/mi-baraja" className="menu-item">MI BARAJA</NavLink>
+        <nav className={`menu ${isMenuOpen ? 'menu-open' : ''}`}>
+            <NavLink to="/cartas" className="menu-item" onClick={handleCloseMenu}>CARTAS</NavLink>
+            <NavLink to="/paquetes" className="menu-item" onClick={handleCloseMenu}>PAQUETES</NavLink>
+            <NavLink to="/centro-de-combate" className="menu-item" onClick={handleCloseMenu}>CENTRO DE COMBATE</NavLink>
+            <NavLink to="/mi-coleccion" className="menu-item" onClick={handleCloseMenu}>MI COLECCION</NavLink>
+            <NavLink to="/mi-baraja" className="menu-item" onClick={handleCloseMenu}>MI BARAJA</NavLink>
         </nav>
     );
 }
