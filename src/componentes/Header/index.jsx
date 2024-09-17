cb44caa81860d584f4062be4100fcc2093190598
GiBsyGG <jcval2801@gmail.com>
2024-09-17 02:26:38 -0500

    Add: Welcome text & Logout

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { IoMdArrowDropdownCircle, IoMdArrowDropupCircle, IoMdLogOut  } from "react-icons/io";
 import BarraBusqueda from '../BarraBusqueda';
 import Menu from '../Menu';
 import './Header.css';
@@ -13,6 +14,7 @@ function Header() {
     const { isAuthenticated, logout, userRole } = useContext(AuthContext); // Cambiado a userRole
     const showSearchBar = location.pathname === '/cartas' || location.pathname === '/paquetes';
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isLogoutButtonVisible, setIsLogoutButtonVisible] = useState(false);
 
     const handleSearch = (query) => {
         actualizarBusqueda(query);
@@ -21,6 +23,10 @@ function Header() {
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+    const toggleLogoutButton = () => {
+        setIsLogoutButtonVisible(!isLogoutButtonVisible);
+    }
 
     // Verificar si el usuario es administrador usando userRole
     const isAdmin = isAuthenticated && userRole === 'admin';
@@ -42,9 +48,25 @@ function Header() {
                 
                 <div className="button-group">
                     {isAuthenticated && (
-                        <NavLink to="/" className="nav-link" onClick={logout}>
-                            <img src="/img/icons/logout.png" alt="Cerrar sesión" />
-                        </NavLink>
+                        <div>
+                            <NavLink className="nav-link" onClick={toggleLogoutButton}>
+                                <div className='user-options-button'>
+                                    {isLogoutButtonVisible && <IoMdArrowDropupCircle className='drop-btn'/>}
+                                    {!isLogoutButtonVisible && <IoMdArrowDropdownCircle className='drop-btn'/>}
+                                    {!isAdmin && <p className='buttons-text'>BIENVENIDO<br/>DUELISTA</p>}
+                                    {isAdmin && <p className='buttons-text'>BIENVENIDO<br/>MASTER</p>}
+                                </div>
+                            </NavLink>
+                            {isLogoutButtonVisible && (
+                                <NavLink to="/" className="nav-link" onClick={logout}>
+                                <div className='user-options-button logout-button'>
+                                    <IoMdLogOut     className='drop-btn'/>
+                                    <p className='buttons-text'>LOGOUT</p>
+                                </div>
+                            </NavLink>
+                            )}
+                        </div>
+                        
                     )}
                     <div className='button-group-user'>
                         {!isAuthenticated && (
