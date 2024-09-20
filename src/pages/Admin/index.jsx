6fc2f949bf27cb07cf7abb1c034ab10ec3fe04d2
GiBsyGG <jcval2801@gmail.com>
2024-09-20 03:03:27 -0500

    Update: Delete Banner admin

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -2,12 +2,11 @@
 import React from 'react';
 import Banner from '@src//componentes/Banner';
 import CategoriaAdmin from '@src//componentes/CategoriaAdmin';
-
+import './Admin.css';
 
 const Admin = () => {
     return (
-        <div>
-          <Banner />
+        <div className='admin_panel'>
           <CategoriaAdmin />
         </div>
       );
