e8d8ba6784a860143ea977d5df555cb6404ceb3a
GiBsyGG <jcval2801@gmail.com>
2024-09-20 03:02:54 -0500

    Update: Home admin

--- src/componentes/CategoriaAdmin/index.jsx
+++ src/componentes/CategoriaAdmin/index.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './CategoriaAdmin.css';
 
+import { TbCards } from "react-icons/tb";
+import { FaUsersCog } from "react-icons/fa";
+import { RiRedPacketLine } from "react-icons/ri";
+
 function CategoriasAdmin() {
     const categorias = [
-        { src: '/img/EditarTienda.png', alt: 'EditarTienda', path: '/cartas-admin' },
-        { src: '/img/EditarUsuariosMobile.png', alt: 'EditarUsuarios', path: '/editar-usuarios' },
+        { icon: <FaUsersCog className='admin_option_icon'/>, alt: 'Editar Usuarios', path: '/editar-usuarios' },
+        { icon: <TbCards className='admin_option_icon'/>, alt: 'Editar Cartas', path: '/cartas-admin' },
+        { icon: <RiRedPacketLine className='admin_option_icon'/>, alt: 'Editar Paquetes', path: '/paquetes-admin' },
     ];
 
     return (
         <div className="categorias">
             {categorias.map((categoria, index) => (
-                <NavLink key={index} to={categoria.path} className="categoria-item">
-                    <img src={categoria.src} alt={categoria.alt} />
+                <NavLink key={index} to={categoria.path} className="categoria-item admin_option">
+                    {categoria.icon}
+                    <p className='admin_option_texto'>{categoria.alt}</p>
                 </NavLink>
             ))}
         </div>
